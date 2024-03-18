066826504c5b0b6b14ba2b9abd9458bd96d51d11
Matheus Azevedo <105979118+AzvdoMatheus@users.noreply.github.com>
2024-03-18 00:28:46 -0300

    Add files via upload

---
+++ src/dell_academy/Classes/Pessoa.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dell_academy.Classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author User
+ */
+public class Pessoa {
+
+  private String nome;
+  private String cpf;
+  private List<Aposta> apostasFeitas;
+
+  public Pessoa(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+    apostasFeitas = new ArrayList<>();
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void adicionarAposta(Aposta aposta) {
+    apostasFeitas.add(aposta);
+}
+
+  public List<Aposta> getApostaFeitas() {
+    return apostasFeitas;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Nome: ").append(nome).append(", ");
+    sb.append("CPF: ").append(cpf);
+    return sb.toString();
+  }
+
+}
---
+++ src/dell_academy/Dell_Academy.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dell_academy;
+
+import dell_academy.UI.Tela_Inicial;
+
+/**
+ *
+ * @author User
+ */
+public class Dell_Academy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Tela_Inicial();
+    }
+    
+}
